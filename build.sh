14b51c3af1d8d8e5f638a4d9fe37483e8b67fd9c
Emil Cieslar <emil.cieslar@gmail.com>
2022-08-25 18:13:15 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,4 @@
 #!/bin/bash -e
 
-if ! type fastlane > /dev/null 2>&1; then
-  if type brew > /dev/null 2>&1; then
-    brew install fastlane
-  else
-    sudo gem install fastlane -NV
-  fi
-fi
-
-script_path=$(cd $(dirname ${0}); pwd)
-cp -r ${script_path}/fastlane ./
-fastlane export_ipa
+bundle install
+bundle exec fastlane export_ipa
